37c6c5a1926dae9493f9c3e1aa244d396fe6bb25
Brandon Sinn <btsinn@users.noreply.github.com>
2020-05-23 16:42:29 -0400

    updated ISSRseq_ReferenceBased.sh
    Modified script to mirror read trimming and negative genome mapping filtering of the latest version of ISSRseq_AssembleReference.sh

--- ISSRseq_ReferenceBased.sh
+++ ISSRseq_ReferenceBased.sh
@@ -4,7 +4,7 @@ echo "
 
 ISSRseq -- ReferenceBased
                        
-development version 0.2
+development version 0.4
 use help for usage 
     
 "
@@ -34,18 +34,22 @@ Dependencies: ABYSS-PE, bbduk
 exit 1
 fi
 
-while getopts "O:I:S:R:T:M:H:P:" opt; do
+while getopts "O:I:S:T:M:H:P:L:K:N:R:" opt; do
 
       case $opt in 
         O) PREFIX=$OPTARG ;;
         I) READ_DIR=$OPTARG ;;
         S) SAMPLE_LIST=$OPTARG ;;
-        R) REF_GENOME=$OPTARG ;;
         T) THREADS=$OPTARG ;;
         M) MIN_LENGTH=$OPTARG ;;
         H) HARD_TRIM=$OPTARG ;;
+        P) ISSR_MOTIF=$OPTARG ;;
+        K) ABYSS_K=$OPTARG ;;
+        L) MIN_CONTIG=$OPTARG ;;
+        N) NEG_REF=$OPTARG ;;
+        R) REF_GENOME=$OPTARG;;
        esac
-done      
+done
 
 #create output directory naming variables
 TIMESTAMP=$(date '+%Y_%m_%d_%H_%M')
@@ -65,11 +69,13 @@ cp $SAMPLE_LIST $OUTPUT_DIR/samples.txt
 while read -r sample
 do
 
-    bbduk in=$READ_DIR/${sample}_R1.fastq in2=$READ_DIR/${sample}_R2.fastq mingc=0.1 maxgc=0.9 forcetrimleft=$HARD_TRIM forcetrimright2=$HARD_TRIM qtrim=t trimq=20 k=18 ktrim=l mink=10 ref=$ISSR_MOTIF minlength=$MIN_LENGTH threads=$THREADS out=$OUTPUT_DIR/trimmed_reads/${sample}_Ltrimmed_R1.fastq out2=$OUTPUT_DIR/trimmed_reads/${sample}_Ltrimmed_R2.fastq >>$OUTPUT_DIR/ISSRseq_read_trimming.log 2>&1
+    bbduk in=$READ_DIR/${sample}_R1.fastq in2=$READ_DIR/${sample}_R2.fastq mingc=0.1 maxgc=0.9 forcetrimleft=$HARD_TRIM forcetrimright2=$HARD_TRIM qtrim=t trimq=10 k=18 tbo=t tpe=t ktrim=l mink=8 mingc=0.1 maxgc=0.9 ref=$ISSR_MOTIF minlength=$MIN_LENGTH threads=$THREADS out=$OUTPUT_DIR/trimmed_reads/${sample}_Ltrimmed_R1.fastq out2=$OUTPUT_DIR/trimmed_reads/${sample}_Ltrimmed_R2.fastq >>$OUTPUT_DIR/ISSRseq_read_trimming.log 2>&1
     
-    bbduk in=$OUTPUT_DIR/trimmed_reads/${sample}_Ltrimmed_R1.fastq in2=$OUTPUT_DIR/trimmed_reads/${sample}_Ltrimmed_R2.fastq minlength=$MIN_LENGTH qtrim=t trimq=20 k=18 ktrim=r mink=10 ref=$ISSR_MOTIF threads=$THREADS mingc=0.1 maxgc=0.9 out=$OUTPUT_DIR/trimmed_reads/${sample}_trimmed_R1.fastq out2=$OUTPUT_DIR/trimmed_reads/${sample}_trimmed_R2.fastq >>$OUTPUT_DIR/ISSRseq_reference_assembly.log 2>&1
+    bbduk in=$OUTPUT_DIR/trimmed_reads/${sample}_Ltrimmed_R1.fastq in2=$OUTPUT_DIR/trimmed_reads/${sample}_Ltrimmed_R2.fastq minlength=$MIN_LENGTH qtrim=t trimq=10 k=18 tbo=t tpe=t ktrim=r mink=8 mingc=0.1 maxgc=0.9 ref=$ISSR_MOTIF threads=$THREADS mingc=0.1 maxgc=0.9 out=$OUTPUT_DIR/trimmed_reads/${sample}_LRtrimmed_R1.fastq out2=$OUTPUT_DIR/trimmed_reads/${sample}_LRtrimmed_R2.fastq >>$OUTPUT_DIR/ISSRseq_reference_assembly.log 2>&1
     
-    rm $OUTPUT_DIR/trimmed_reads/${sample}_Ltrimmed_R1.fastq $OUTPUT_DIR/trimmed_reads/${sample}_Ltrimmed_R2.fastq
+    bbmap threads=$THREADS nodisk=f killbadpairs=t slow=t ref=$NEG_REF in=$OUTPUT_DIR/trimmed_reads/${sample}_LRtrimmed_R1.fastq in2=$OUTPUT_DIR/trimmed_reads/${sample}_LRtrimmed_R2.fastq outu=$OUTPUT_DIR/trimmed_reads/${sample}_trimmed_R1.fastq outu2=$OUTPUT_DIR/trimmed_reads/${sample}_trimmed_R2.fastq statsfile=stderr >>$OUTPUT_DIR/ISSRseq_reference_assembly.log 2>&1
+
+    rm $OUTPUT_DIR/trimmed_reads/${sample}_Ltrimmed_R1.fastq $OUTPUT_DIR/trimmed_reads/${sample}_Ltrimmed_R2.fastq $OUTPUT_DIR/trimmed_reads/${sample}_LRtrimmed_R1.fastq $OUTPUT_DIR/trimmed_reads/${sample}_LRtrimmed_R2.fastq
     
 echo ""${sample}" reads processed"
 
